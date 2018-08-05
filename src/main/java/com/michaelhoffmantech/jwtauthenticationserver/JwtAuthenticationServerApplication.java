866b05048bca72dc5e369db3d2ee77b3f0cea42c
Michael Hoffman <mike@michaelhoffmaninc.com>
2018-08-05 12:04:30 -0500

    Initial commit

---
+++ src/main/java/com/michaelhoffmantech/jwtauthenticationserver/JwtAuthenticationServerApplication.java
@@ -0,0 +1,12 @@
+package com.michaelhoffmantech.jwtauthenticationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/michaelhoffmantech/jwtauthenticationserver/JwtAuthenticationServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.michaelhoffmantech.jwtauthenticationserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwtAuthenticationServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
